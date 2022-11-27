66d3059de302a1a43e08e049273e126681ddc63e
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-11-26 21:24:45 -0800

    init with contracts and react

---
+++ smart-contracts/src/IShieldWallet.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT only
+
+pragma solidity ^0.8.0;
+
+interface IWallet {
+
+    function owner() external view returns (address);
+
+    function services() external view returns (uint);
+
+    function transferOwner(address _newOwner) external;
+
+    function authorised(address _service) external view returns (bool);
+
+    function enabled(bytes4 _sig) external view returns (address);
+
+    function authorizeService(address _service, bool _value) external;
+
+    function enableStaticCall(address _module, bytes4 _method) external;
+}
