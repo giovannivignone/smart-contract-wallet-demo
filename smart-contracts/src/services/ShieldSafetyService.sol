f01e5f7ec2ddbcbc31e9dfe0a7eb304d40101a44
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-11-28 10:30:45 -0500

    init of ShieldSafetyService

---
+++ smart-contracts/src/services/ShieldSafetyService.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT only
+
+pragma solidity ^0.8.0;
+
+import "./BaseService.sol";
+import "../IWallet.sol";
+
+/**
+ * @title ShieldSafetyService
+ * @dev Service to manage the security aspects of a CoinMaster Wallet.
+ * @notice This is a singleton contract to manage all CoinMaster Wallets based on storage data
+*/
+abstract contract ShieldSafetyService is BaseService {
+    function lockWallet(address _wallet) external onlyWalletOwnerOrSelf(_wallet) onlyWhenUnlocked(_wallet) {
+        IWallet(_wallet).lock(true);
+    }
+}
