0af35021c5aeec03b5bac1caf2aa735e95a74e7d
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-11-29 01:07:39 -0500

    finalized css for wallet builder

--- shield-wallet-frontend/src/modules/WalletBuilderContent.js
+++ shield-wallet-frontend/src/modules/WalletBuilderContent.js
@@ -3,7 +3,10 @@ import { InputBox } from "modules/InputBox";
 
 export const WalletBuilderContent = () => {
     return (
-        <div className="Backdrop CenteredContent"> 
+        <div className="Backdrop CenteredContent">
+            <div className="TopBar">
+                <div className="InnerTopBarText">Wallet Builder</div>
+            </div>
             <div className="WalletBuilderWrapper">
                 <InputBox inputTitle="Wallet Name" inputPlaceHolder="Business Wallet"/>
                 <InputBox inputTitle={"Guardians "} inputPlaceHolder="0xA87D...,0x90WGs..." />
