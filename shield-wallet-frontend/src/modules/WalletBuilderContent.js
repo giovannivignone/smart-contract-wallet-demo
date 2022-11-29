5832c7e49fd0a6a7a57253dfcbb5abb91bf875c7
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-11-29 03:52:57 -0500

    modify jsons

--- shield-wallet-frontend/src/modules/WalletBuilderContent.js
+++ shield-wallet-frontend/src/modules/WalletBuilderContent.js
@@ -1,5 +1,7 @@
 import "styles/inputBox.css";
 import { InputBox } from "modules/InputBox";
+import Web3 from "web3";
+
 
 export const WalletBuilderContent = () => {
     const fetchWalletInputs = () => {
@@ -8,11 +10,27 @@ export const WalletBuilderContent = () => {
         const threshold = document.getElementById("walletThreshold").value;
         return [name, guardians, threshold];
     }
-    const deployWallet = () => {
-        console.log("Deploying Wallet");
-        document.getElementById("deployWalletButton").disabled = true;
+    const fetchWeb3 = async() => {
+        if (!window.ethereum) {
+            alert("Please install MetaMask to use this dApp!");
+            throw Error("MetaMask not installed");
+        }
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        return web3;
+    }
+    const createWallet = async () => {
+        const web3 = await fetchWeb3();
         const [name, guardians, threshold] = fetchWalletInputs();
-        console.log(name, guardians, threshold);
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const tx = await contract.methods.createWallet(name, guardians, threshold).send({ from: account });
+        return tx;
+    }
+    const handleCreateWallet = async() => {
+        document.getElementById("deployWalletButton").disabled = true;
+        const executedTransaction = await createWallet();
     }
     return (
         <div className="Backdrop CenteredContent">
@@ -24,7 +42,7 @@ export const WalletBuilderContent = () => {
                 <InputBox inputTitle={"Guardians"} id="walletGuardians" inputPlaceHolder="0xA87D...,0x90WGs..." />
                 <InputBox inputTitle={"Guardian Voting Weights"} id="walletGuardianWeights" inputPlaceHolder="1,2,..." />
                 <InputBox inputTitle={"Vote Threshold"} id="walletThreshold" inputPlaceHolder="2" />
-                <button className="Deploy" id="deployWalletButton" onClick={deployWallet}>Deploy Wallet</button>
+                <button className="Deploy" id="deployWalletButton" onClick={handleCreateWallet}>Deploy Wallet</button>
             </div>
         </div>
     );
