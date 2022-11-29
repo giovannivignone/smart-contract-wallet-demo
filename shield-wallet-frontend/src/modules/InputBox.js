4cdbc8929f32ae15a20bf22d3d107631251d560c
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-11-29 01:35:51 -0500

    fetching inputs

--- shield-wallet-frontend/src/modules/InputBox.js
+++ shield-wallet-frontend/src/modules/InputBox.js
@@ -1,12 +1,11 @@
 import "styles/inputBox.css";
-import { FC } from "react"
 
-export const InputBox = ({inputTitle, inputPlaceHolder}) => {
+export const InputBox = ({inputTitle, inputPlaceHolder, id}) => {
     return (
         <fieldset>
             <legend>{inputTitle}</legend>
             <div>
-                <input placeholder={inputPlaceHolder} list="plate-number-list" id="exampleDataList"></input>
+                <input placeholder={inputPlaceHolder} list="plate-number-list" className="exampleDataList" id={id}></input>
             </div>
         </fieldset>
     );
--- shield-wallet-frontend/src/modules/WalletBuilderContent.js
+++ shield-wallet-frontend/src/modules/WalletBuilderContent.js
@@ -2,17 +2,29 @@ import "styles/inputBox.css";
 import { InputBox } from "modules/InputBox";
 
 export const WalletBuilderContent = () => {
+    const fetchWalletInputs = () => {
+        const name = document.getElementById("walletName").value;
+        const guardians = document.getElementById("walletGuardians").value;
+        const threshold = document.getElementById("walletThreshold").value;
+        return [name, guardians, threshold];
+    }
+    const deployWallet = () => {
+        console.log("Deploying Wallet");
+        document.getElementById("deployWalletButton").disabled = true;
+        const [name, guardians, threshold] = fetchWalletInputs();
+        console.log(name, guardians, threshold);
+    }
     return (
         <div className="Backdrop CenteredContent">
             <div className="TopBar">
                 <div className="InnerTopBarText">Wallet Builder</div>
             </div>
             <div className="WalletBuilderWrapper">
-                <InputBox inputTitle="Wallet Name" inputPlaceHolder="Business Wallet"/>
-                <InputBox inputTitle={"Guardians "} inputPlaceHolder="0xA87D...,0x90WGs..." />
-                <InputBox inputTitle={"Guardian Voting Weights"} inputPlaceHolder="1,2,..." />
-                <InputBox inputTitle={"Vote Threshold"} inputPlaceHolder="2" />
-                <button className="Deploy">Deploy Wallet</button>
+                <InputBox inputTitle="Wallet Name" id="walletName" inputPlaceHolder="Business Wallet"/>
+                <InputBox inputTitle={"Guardians"} id="walletGuardians" inputPlaceHolder="0xA87D...,0x90WGs..." />
+                <InputBox inputTitle={"Guardian Voting Weights"} id="walletGuardianWeights" inputPlaceHolder="1,2,..." />
+                <InputBox inputTitle={"Vote Threshold"} id="walletThreshold" inputPlaceHolder="2" />
+                <button className="Deploy" id="deployWalletButton" onClick={deployWallet}>Deploy Wallet</button>
             </div>
         </div>
     );
